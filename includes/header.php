1aaf518d92e88cab4c004a958fb7b4525e3978ba
aomariaymane <jaycitmse@gmail.com>
2024-01-12 22:29:41 +0100

    ''

--- includes/header.php
+++ includes/header.php
@@ -8,12 +8,14 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/bootstrap.min.css.map">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/Main.css">
     <link rel="stylesheet" href="css/collections.css">
     <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="js/script.js">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/Style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <link rel="stylesheet" href="css/products.css">
-    
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/registre.css">
+    <link rel="stylesheet" href="css/Login.css">
 </head>
